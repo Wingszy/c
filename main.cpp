b19bf8b097401872911e39ee60b50f5643be85a3
Wingszy <85103070+Wingszy@users.noreply.github.com>
2021-11-16 14:40:47 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include"QUOTE.h"
+using namespace std;
+int main(int argc, char**argv)
+{
+	Quote b1("Áú×åI", 128);
+	//bulk_quote b2(string("Áú×åII"), 128.0, 10, 0.7);
+	print_total(cout, b1, 10);
+	//print_total(cout, b2, 10);
+	system("pause");
+	return 0;
+}
